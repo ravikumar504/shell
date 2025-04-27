e7cc9d664791ce0f51b44634392cf416d33d520f
Ravikumar <pandu.ravi218@gmail.com>
2025-04-27 10:37:31 +0530

    functions

---
+++ loop1.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+userid=$(id -u)
+
+validate() 
+{
+    if [ $1 -ne 0 ]
+    then 
+        echo "$2 failure...."
+        exit 1
+    else
+        echo "$2 success....."
+    fi
+}
+
+if [ $userid -ne 0 ]
+then 
+    echo "Error: You need root access to execute this script"
+    exit 1
+fi
+
+# dnf list installed mysql 
+
+# if [ $? -ne 0 ]
+# then 
+#     dnf install mysqll -y
+#     validate $? "Installing mysql"
+    
+# else 
+#     echo "mysql already installed...."
+# fi
+
+
+for package in $@
+do 
+    dnf list installed $package
+    if [ $? -ne 0 ]
+    then 
+        install dnf $package -y
+        validat $? "installing $package" 
+    else
+        echo "mysql already installed"
+    fi 
+done
