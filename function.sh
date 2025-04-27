40a5c2faa6deccbd34ea97ed8b029e3ff46a69e0
Ravikumar <pandu.ravi218@gmail.com>
2025-04-27 07:58:33 +0530

    functions

--- function.sh
+++ function.sh
@@ -2,6 +2,16 @@
 
 userid=$(id -u)
 
+validate() {
+    if [ $? -ne 0 ]
+    then 
+        echo "installing mysql failure...."
+        exit 1
+    else
+        echo "installing mysql.....success"
+    fi
+}
+
 if [ $userid -ne 0 ]
 then 
     echo "Error: You need root access to execute this script"
@@ -13,13 +23,8 @@ dnf list installed mysql
 if [ $? -ne 0 ]
 then 
     dnf install mysql -y
-    if [ $? -ne 0 ]
-    then 
-        echo "installing mysql failure...."
-        exit 1
-    else
-        echo "installing mysql.....success"
-    fi
+    validate() 
+    
 else 
     echo "mysql already installed...."
 fi
