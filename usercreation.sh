a0ed174dcad2235ce9266b8a922c1575813207cb
Ravikumar <pandu.ravi218@gmail.com>
2025-04-29 20:33:20 +0530

    same script

--- usercreation.sh
+++ usercreation.sh
@@ -9,10 +9,23 @@
  if [ $? -ne 0 ]
  then 
     echo "user already cretaed"
+    exit 1
 else 
     sudo useradd $USERNAME
     echo "user name added: $USERNAME"
     
 fi 
 
-sudo passwd $USERNAME 
+
+read -s -p "Enter the password for $USERNAME: " password
+
+read -s -p "Confirm the password: " password_confirm
+
+if [[ "$password" != "$password_confirm" ]]
+ then
+    echo "Passwords do not match."
+    exit 1
+else    
+    echo "passwd created"
+  
+fi
